a6cd89d798f8ab79d5e328c0ebe3cb8defb5fe79
Your Name <you@example.com>
2024-04-01 00:02:51 +0530

    v1.1

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/django_hash_make_password', views.django_hash_make_password, name='django_hash_make_password'),
+    path('api/django_hash_check_password', views.django_hash_check_password, name='django_hash_check_password'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,4 +4,19 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def django_hash_make_password(request):
-    return JsonResponse({'message':'Make Password Called'})
+    if request.method == 'POST':
+        password = request.POST.get('password')
+        hashed_password = make_password(password)
+        return JsonResponse({'message':f'{hashed_password}', 'message_hint':'hash_generated'})
+    else:
+        return JsonResponse({'message':'Methods not allowed'}, status=400)
+    
+@csrf_exempt
+def django_hash_check_password(request):
+    if request.method == 'POST':
+        password = request.POST.get('password')
+        hashed_password = request.POST.get('hashed_password')
+        is_valid = check_password(password, hashed_password)
+        return JsonResponse({'message':f'{is_valid}', 'message_hint':'password_verified'})
+    else:
+        return JsonResponse({'message':'Methods not allowed'}, status=400)

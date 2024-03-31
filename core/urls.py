dec8e085c991fc748c34fda807fd368fc1b4d0a3
Your Name <you@example.com>
2024-04-01 00:59:39 +0530

    v1.3

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_page, name='home_page'),
     path('api/django_hash_make_password', views.django_hash_make_password, name='django_hash_make_password'),
     path('api/django_hash_check_password', views.django_hash_check_password, name='django_hash_check_password'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,17 +6,32 @@ from django.views.decorators.csrf import csrf_exempt
 def django_hash_make_password(request):
     if request.method == 'POST':
         password = request.POST.get('password')
+        if not password:
+            return JsonResponse({'message': 'Password is required'}, status=400)
+        
         hashed_password = make_password(password)
-        return JsonResponse({'message':f'{hashed_password}', 'message_hint':'hash_generated'})
+        if hashed_password:
+            return JsonResponse({'message': f'{hashed_password}', 'message_hint':'hash_generated'}, status=201)
+        else:
+            return JsonResponse({'message': 'Error while generating hash please try again'}, status=500)
     else:
-        return JsonResponse({'message':'Methods not allowed'}, status=400)
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
     
 @csrf_exempt
 def django_hash_check_password(request):
     if request.method == 'POST':
         password = request.POST.get('password')
         hashed_password = request.POST.get('hashed_password')
-        is_valid = check_password(password, hashed_password)
-        return JsonResponse({'message':f'{is_valid}', 'message_hint':'password_verified'})
+        
+        if not password or not hashed_password:
+            return JsonResponse({'message': 'Both password and hashed_password are required'}, status=400)
+        
+        if check_password(password, hashed_password):
+            return JsonResponse({'message': 'Password Verified', 'message_hint':'password_verified'}, status=200)
+        else:
+            return JsonResponse({'message': 'Password Mismatch'}, status=400)
     else:
-        return JsonResponse({'message':'Methods not allowed'}, status=400)
+        return JsonResponse({'message': 'Method not allowed'}, status=405)
+    
+def home_page(request):
+    return JsonResponse({'message': 'Welcome to Django Hasher API'})

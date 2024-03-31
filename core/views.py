df5c0e162303b75e2ce45f71dfef27f88e40aecc
Your Name <you@example.com>
2024-03-31 23:49:10 +0530

    @csrf_exempt

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.hashers import make_password, check_password
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def django_hash_make_password(request):
     return JsonResponse({'message':'Make Password Called'})
